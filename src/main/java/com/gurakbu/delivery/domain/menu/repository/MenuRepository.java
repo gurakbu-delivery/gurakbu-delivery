3ecd423785f7ca16b59da9a643166159780e9271
Nosilv <nosilv8@gmail.com>
2025-03-04 11:38:46 +0900

    feat: 메뉴 repository 생성

---
+++ src/main/java/com/gurakbu/delivery/domain/menu/repository/MenuRepository.java
@@ -0,0 +1,11 @@
+package com.gurakbu.delivery.domain.menu.repository;
+
+import com.gurakbu.delivery.domain.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+    // 특정 레스토랑에 해당 메뉴가 존재하는지 확인하는 메서드: Optional<Menu> 반환 (존재하지 않으면 Optional.empty())
+    // Optional<Menu> findByRestaurentIdAndId(Long restaurentId, Long id);
+}
