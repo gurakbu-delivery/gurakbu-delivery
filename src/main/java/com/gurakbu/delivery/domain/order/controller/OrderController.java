4f1dd14e4426e2be1ecb67bc99790621ce134868
Kim Hani <rlahani99@gmail.com>
2025-03-04 13:47:57 +0900

    주문 기능 구현 완료

--- src/main/java/com/gurakbu/delivery/domain/order/controller/OrderController.java
+++ src/main/java/com/gurakbu/delivery/domain/order/controller/OrderController.java
@@ -1,7 +1,62 @@
 package com.gurakbu.delivery.domain.order.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.gurakbu.delivery.domain.order.dto.request.OrderRequestDto;
+import com.gurakbu.delivery.domain.order.dto.request.OrderStatusUpdateRequestDto;
+import com.gurakbu.delivery.domain.order.dto.response.OrderResponseDto;
+import com.gurakbu.delivery.domain.order.service.OrderService;
+import com.gurakbu.delivery.domain.order.status.OrderStatus;
+import com.gurakbu.delivery.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(
+            @AuthenticationPrincipal User user,
+            @RequestBody OrderRequestDto orderRequestDto){
+
+        OrderResponseDto orderResponseDto = orderService.createOrder(user, orderRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponseDto);
+    }
+
+
+    // 주문 상태 변경
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<OrderStatus> updateOrderStatus(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long orderId,
+            @RequestBody OrderStatusUpdateRequestDto requestDto){
+
+        OrderStatus updatedStatus = orderService.updateOrderStatus(user,orderId,requestDto.getStatus());
+
+        return ResponseEntity.ok(updatedStatus);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponseDto> getOrder(@PathVariable Long orderId){
+        OrderResponseDto responseDto = orderService.getOrder(orderId);
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+
+    @DeleteMapping("{orderId}/cancle")
+    public ResponseEntity<String> cancelOrder(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long orderId
+    ){
+
+        orderService.cancelOrder(user,orderId);
+        return ResponseEntity.ok("Order cancelled successfully");
+    }
+
 }
