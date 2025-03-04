ca74a2c70beb8dcfff8f73a92f96957c8deb370a
Nosilv <nosilv8@gmail.com>
2025-03-04 21:04:52 +0900

    fix: Restaurant Category와 다른 결의 MenuCategory ENUM으로 수정

--- src/main/java/com/gurakbu/delivery/domain/menu/entity/MenuCategory.java
+++ src/main/java/com/gurakbu/delivery/domain/menu/entity/MenuCategory.java
@@ -1,25 +1,16 @@
 package com.gurakbu.delivery.domain.menu.entity;
 
 public enum MenuCategory {
-    KOREAN,        // 한식
-    CHINESE,       // 중식
-    JAPANESE,      // 일식
-    WESTERN,       // 양식
-    FAST_FOOD,     // 패스트푸드
-    CHICKEN,       // 치킨
-    PIZZA,         // 피자
-    BURGER,        // 버거
-    SNACK,         // 분식
-    ASIAN,         // 아시안 음식 (베트남, 태국 등)
-    STEW,          // 찌개 & 전골
-    RICE_BOWL,     // 덮밥
-    SANDWICH,      // 샌드위치
-    NOODLE,        // 국수 & 면류
-    SALAD,         // 샐러드
-    BBQ,           // 바비큐 & 구이
-    SEAFOOD,       // 해산물
-    DESSERT,       // 디저트 & 베이커리
-    CAFE,          // 카페 & 음료
-    VEGAN,         // 채식
-    ETC            // 기타
+    MAIN,       // 메인 메뉴
+    SIDE,       // 사이드 메뉴
+    DRINK,      // 음료
+    ALCOHOL,    // 주류
+    EVENT,      // 이벤트 메뉴
+    ETC;        // 기타
+
+    // Enum -> String으로 변환 (JSON 변환 시 필요)
+    @Override
+    public String toString() {
+        return name();
+    }
 }
