f6000f5d6a0153904cbf7d7eeba7faddd3667e32
Nosilv <nosilv8@gmail.com>
2025-03-04 11:30:21 +0900

    feat: 메뉴 entity 생성

---
+++ src/main/java/com/gurakbu/delivery/domain/menu/entity/MenuCategory.java
@@ -0,0 +1,25 @@
+package com.gurakbu.delivery.domain.menu.entity;
+
+public enum MenuCategory {
+    KOREAN,        // 한식
+    CHINESE,       // 중식
+    JAPANESE,      // 일식
+    WESTERN,       // 양식
+    FAST_FOOD,     // 패스트푸드
+    CHICKEN,       // 치킨
+    PIZZA,         // 피자
+    BURGER,        // 버거
+    SNACK,         // 분식
+    ASIAN,         // 아시안 음식 (베트남, 태국 등)
+    STEW,          // 찌개 & 전골
+    RICE_BOWL,     // 덮밥
+    SANDWICH,      // 샌드위치
+    NOODLE,        // 국수 & 면류
+    SALAD,         // 샐러드
+    BBQ,           // 바비큐 & 구이
+    SEAFOOD,       // 해산물
+    DESSERT,       // 디저트 & 베이커리
+    CAFE,          // 카페 & 음료
+    VEGAN,         // 채식
+    ETC            // 기타
+}
---
+++ src/main/java/com/gurakbu/delivery/domain/menu/entity/MenuStatus.java
@@ -0,0 +1,7 @@
+package com.gurakbu.delivery.domain.menu.entity;
+
+public enum MenuStatus {
+    AVALIABLE,      // 판매가능
+    UNAVAILABLE,    // 판매중단
+    DELETED         // 삭제
+}
