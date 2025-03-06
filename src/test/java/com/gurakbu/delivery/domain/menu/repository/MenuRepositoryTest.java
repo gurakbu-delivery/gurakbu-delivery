450ec1e2429b9e93d323424195f100c2467d056d
Nosilv <nosilv8@gmail.com>
2025-03-07 04:33:22 +0900

    test: menu쪽 테스트 완료 & 99% 성공

--- src/test/java/com/gurakbu/delivery/domain/menu/repository/MenuRepositoryTest.java
+++ src/test/java/com/gurakbu/delivery/domain/menu/repository/MenuRepositoryTest.java
@@ -18,7 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.swing.text.html.Option;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,44 +41,55 @@ class MenuRepositoryTest {
     private UserRepository userRepository;
 
     private Restaurant restaurant1;
+    private Restaurant restaurant2;
     private Menu menu1;
+    private Menu menu2;
     private User user1;
 
     @BeforeEach
     void setUp() {
-        // 1. 테스트용 사용자 저장
-        user1 = new User("abc1234@naver.com", "qlalfqjsgh1!","김미미","01011111111" , UserRole.OWNER);
-        user1=userRepository.save(user1); // 영속화
+        // 1. 테스트용 유저 생성
+        user1 = userRepository.save(new User("asdf@naver.com", "qlalfqjsgh1!","김미미", "01011111111", UserRole.ADMIN));
 
-        // 2. 테스트용 레스토랑 저장
+        // 2. 테스트용 가게 저장
         restaurant1 = new Restaurant("야채구락부", "스파르타읍 내배캠면 스프링리", "온 가족이 먹는 음식", RestaurantCategory.KOREAN, RestaurantStatus.OPEN, LocalTime.of(9,0), LocalTime.of(22,0),5000);
+        restaurant1.setUser(user1);
         restaurant1 = restaurantRepository.save(restaurant1); // 영속화
+
+        restaurant2 = new Restaurant("고기구락부", "스파르타읍 내배캠면 스프링리", "온 가족이 먹는 음식", RestaurantCategory.KOREAN, RestaurantStatus.OPEN, LocalTime.of(9,0), LocalTime.of(22,0),5000);
+        restaurant2.setUser(user1);
+        restaurant2 = restaurantRepository.save(restaurant2); // 영속화
+
+
 
         // 3. 테스트용 메뉴 저장
         menu1 = Menu.builder()
                 .restaurant(restaurant1)
                 .name("장충동왕왕족발보쌈")
                 .price(30000)
-                .category(MenuCategory.SIDE)
-                .popularity(false)
+                .category(MenuCategory.MAIN)
+                .popularity(Boolean.FALSE)
                 .status(MenuStatus.OPEN)
                 .build();
         menu1 = menuRepository.save(menu1);
-    }
 
-    @AfterEach
-    void tearDown() {
-        userRepository.deleteAll();
-        menuRepository.deleteAll();
-        restaurantRepository.deleteAll();
+        menu2 = Menu.builder()
+                .restaurant(restaurant1)
+                .name("막국수")
+                .price(10000)
+                .category(MenuCategory.SIDE)
+                .popularity(Boolean.TRUE)
+                .status(MenuStatus.OPEN)
+                .build();
+        menu2 = menuRepository.save(menu2);
     }
 
     @Test
-    @DisplayName("특정 레스토랑의 특정 메뉴 찾기 - 성공")
     void 레스토랑id와_메뉴id로_특정_메뉴를_조회할_수_있다(){
         // given
 
         // when
+        System.out.println(restaurant1.getId()+", "+ menu1.getId());
         Optional<Menu> foundMenu = menuRepository.findByRestaurantIdAndId(restaurant1.getId(), menu1.getId());
 
         // then
@@ -92,7 +105,6 @@ class MenuRepositoryTest {
     }
 
     @Test
-    @DisplayName("특정 레스토랑의 특정 메뉴 찾기 - 존재하지 않는 레스토랑")
     void 레스토랑이_존재하지_않아_특정_메뉴를_조회할_수_없다(){
         // given
 
@@ -104,7 +116,6 @@ class MenuRepositoryTest {
     }
 
     @Test
-    @DisplayName("특정 레스토랑의 특정 메뉴 찾기 - 존재하지 않는 메뉴")
     void 존재하지_않는_메뉴는_조회할_수_없다(){
         // given
 
@@ -113,5 +124,29 @@ class MenuRepositoryTest {
 
         // then
         assertThat(foundMenu).isEmpty();
+    }
+
+    @Test
+    void 특정_가게의_전체_메뉴를_조회한다(){
+        // given
+
+        // when
+        Optional<List<Menu>> foundMenus = menuRepository.findByRestaurantId(restaurant1.getId());
+
+        // then
+        assertThat(foundMenus).isPresent();                 // 메뉴 리스트의 존재 확인
+        assertThat(foundMenus.get()).hasSize(2);   // 저장된 메뉴의 개수(2개) 확인
+    }
+
+    @Test
+    void 특정_가게의_메뉴가_없는데_조회할_때는_빈_리스트를_반환한다(){
+        // given
+
+        // when
+        Optional<List<Menu>> foundMenus = menuRepository.findByRestaurantId(restaurant2.getId());
+
+        // then
+        assertThat(foundMenus).isPresent();                 // 메뉴 리스트의 존재 확인
+        assertThat(foundMenus.get()).isEmpty();             // 빈 리스트인지 확인
     }
 }
