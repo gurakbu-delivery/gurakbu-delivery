fec65083b8c11a0fba3ed44d9e43c8962000d929
Kim-WonJoon <skygood96@gmail.com>
2025-03-07 11:59:44 +0900

    service 테스트코드

--- src/main/java/com/gurakbu/delivery/domain/user/dto/request/LoginRequestDto.java
+++ src/main/java/com/gurakbu/delivery/domain/user/dto/request/LoginRequestDto.java
@@ -2,11 +2,15 @@ package com.gurakbu.delivery.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     @Email(message = "올바른 이메일 형식을 입력해주세요.")
